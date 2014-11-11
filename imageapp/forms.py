ea5a03c82fab3c1a60312bac687ae7f3c805913e
Scaria <scaria.ar@gmail.com>
2014-11-11 16:24:58 -0700

    Scaria - UI

--- imageapp/forms.py
+++ imageapp/forms.py
@@ -6,12 +6,14 @@ from imageapp.models import Settings
 
 class LoginForm(forms.Form):
     email = forms.EmailField(label='Email', max_length=200)
-    password = forms.CharField(label='Password', max_length=200)
+    password = forms.CharField(label='Password', max_length=200, widget=forms.PasswordInput())
+
 
 class RegisterForm(ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'second_name', 'password')
+        widgets = {'password': forms.PasswordInput(),}
 
 
 class SettingsForm(forms.Form):
