f1002f9b1383f8851d1e10e71268ca35e35faea1
Scaria <scaria.ar@gmail.com>
2014-11-11 18:41:31 -0700

    Scaria - Heroku wsgi

--- image_space/wsgi.py
+++ image_space/wsgi.py
@@ -6,9 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "image_space.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
+
